358797dbd816b243bfb97dc064e81a00e4d83561
Simon Francies <simon_francies@hotmail.com>
2019-03-31 19:37:03 +0100

    Access unit tests

--- ShoppingCartApi/test/ShoppingCartApi.UnitTests/AccessTests/OrderAccessTests.cs
+++ ShoppingCartApi/test/ShoppingCartApi.UnitTests/AccessTests/OrderAccessTests.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using ShoppingCartApi.Access;
+using ShoppingCartApi.Common.Exceptions;
+using ShoppingCartApi.Common.Models;
 using ShoppingCartApi.UnitTests.DbFixtures;
 using Xunit;
 
@@ -8,5 +12,64 @@ namespace ShoppingCartApi.UnitTests.AccessTests
 {
     public class OrderAccessTests : ShoppingCartFixtureContext, IClassFixture<ShoppingCartEfDatabaseFixture>
     {
+        [Fact]
+        public void CanGetAllOrders()
+        {
+            OrderAccess sut = CreateSystemUnderTest();
+
+            IReadOnlyCollection<Order> orders = sut.GetAllOrders();
+
+            Assert.NotEmpty(orders);
+        }
+
+        [Fact]
+        public void CanGetOrderById()
+        {
+            OrderAccess sut = CreateSystemUnderTest();
+
+            Order order = sut.GetOrder(Guid.Parse("51B5CFF8-B2CE-4DE8-AB8A-16B6343E105A"));
+
+            Assert.NotNull(order);
+        }
+
+        [Fact]
+        public void CanGetCustomerOrders()
+        {
+            OrderAccess sut = CreateSystemUnderTest();
+
+            IReadOnlyCollection<Order> orders =
+                sut.GetCustomerOrders(Guid.Parse("56DEF7D9-061C-4A26-BDF6-81695B074DD7"));
+
+            Assert.NotEmpty(orders);
+        }
+
+        [Fact]
+        public async Task CanCreateOrder()
+        {
+            OrderAccess sut = CreateSystemUnderTest();
+
+            Order order = await sut.CreateOrderAsync(Guid.Parse("56DEF7D9-061C-4A26-BDF6-81695B074DD7"));
+
+            Assert.NotNull(order);
+        }
+
+        [Fact]
+        public async Task ThrowsErrorWhenCreatingOrderForNonCustomer()
+        {
+            OrderAccess sut = CreateSystemUnderTest();
+
+            await Assert.ThrowsAsync<CustomerNotFoundException>(async () => await sut.CreateOrderAsync(Guid.NewGuid()));
+        }
+
+
+        [Fact]
+        public async Task CanClearOrder()
+        {
+            OrderAccess sut = CreateSystemUnderTest();
+
+            await sut.ClearOrderAsync(Guid.Parse("51B5CFF8-B2CE-4DE8-AB8A-16B6343E105A"));
+        }
+
+        private OrderAccess CreateSystemUnderTest() => new OrderAccess(CreateContext);
     }
 }
--- ShoppingCartApi/test/ShoppingCartApi.UnitTests/AccessTests/OrderItemAccessTests.cs
+++ ShoppingCartApi/test/ShoppingCartApi.UnitTests/AccessTests/OrderItemAccessTests.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using AutoFixture;
+using Moq;
+using ShoppingCartApi.Access;
+using ShoppingCartApi.Common.Exceptions;
+using ShoppingCartApi.Common.Models;
 using ShoppingCartApi.UnitTests.DbFixtures;
 using Xunit;
 
@@ -8,5 +14,88 @@ namespace ShoppingCartApi.UnitTests.AccessTests
 {
     public class OrderItemAccessTests : ShoppingCartFixtureContext, IClassFixture<ShoppingCartEfDatabaseFixture>
     {
+        private readonly Fixture _fixture = new Fixture();
+
+        [Fact]
+        public void CanGetOrderItem()
+        {
+            OrderItemAccess sut = CreateSystemUnderTest();
+
+            OrderItem orderItem = sut.GetOrderItem(Guid.Parse("BB774E9D-CB0A-41FE-99BD-97780CA49217"));
+
+            Assert.NotNull(orderItem);
+        }
+
+        [Fact]
+        public async Task CanCreateOrderItem()
+        {
+            OrderItemAccess sut = CreateSystemUnderTest();
+
+            OrderItem orderItem = await sut.CreateOrderItemAsync(Guid.Parse("51B5CFF8-B2CE-4DE8-AB8A-16B6343E105A"),
+                Guid.Parse("C31A24E6-54E7-4644-B62E-C587001ED18F"), 4);
+
+            Assert.NotNull(orderItem);
+        }
+
+        [Fact]
+        public async Task ThrowsErrorWhenCreatingOrderItemForNonOrder()
+        {
+            OrderItemAccess sut = CreateSystemUnderTest();
+
+            await Assert.ThrowsAsync <OrderNotFoundException>(async () => await sut.CreateOrderItemAsync(
+                Guid.NewGuid(),
+                Guid.Parse("C31A24E6-54E7-4644-B62E-C587001ED18F"), 4));
+        }
+
+        [Fact]
+        public async Task ThrowsErrorWhenCreatingOrderItemForNonProduct()
+        {
+            OrderItemAccess sut = CreateSystemUnderTest();
+
+            await Assert.ThrowsAsync<ProductNotFoundException>(async () => await sut.CreateOrderItemAsync(
+                Guid.Parse("51B5CFF8-B2CE-4DE8-AB8A-16B6343E105A"),
+                Guid.NewGuid(), 4));
+        }
+
+        [Fact]
+        public async Task CanUpdateOrderItem()
+        {
+            int quantity = _fixture.Create<int>();
+            OrderItemAccess sut = CreateSystemUnderTest();
+
+            OrderItem orderItem =
+                await sut.UpdateOrderItemQuantityAsync(Guid.Parse("43E4BA9F-5D82-4414-9702-5CA3DEF48168"), quantity);
+
+            Assert.Equal(quantity, orderItem.Quantity);
+        }
+
+        [Fact]
+        public async Task ThrowsErrorWhenUpdatingNonOrderItem()
+        {
+            OrderItemAccess sut = CreateSystemUnderTest();
+
+            await Assert.ThrowsAsync<OrderItemNotFoundException>(async () => await sut.UpdateOrderItemQuantityAsync(
+                Guid.NewGuid(), It.IsAny<int>()));
+        }
+
+        [Fact]
+        public async Task CanDeleteOrderItem()
+        {
+            int quantity = _fixture.Create<int>();
+            OrderItemAccess sut = CreateSystemUnderTest();
+
+            await sut.RemoveOrderItemAsync(Guid.Parse("43E4BA9F-5D82-4414-9702-5CA3DEF48168"));
+        }
+
+        [Fact]
+        public async Task ThrowsErrorWhenDeletingNonOrderItem()
+        {
+            OrderItemAccess sut = CreateSystemUnderTest();
+
+            await Assert.ThrowsAsync<OrderItemNotFoundException>(async () => await sut.RemoveOrderItemAsync(
+                Guid.NewGuid()));
+        }
+
+        private OrderItemAccess CreateSystemUnderTest() => new OrderItemAccess(CreateContext);
     }
 }
