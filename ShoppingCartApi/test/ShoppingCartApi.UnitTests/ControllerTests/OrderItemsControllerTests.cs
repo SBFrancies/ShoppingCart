0c2b7ffed24df1d369cb9800b9a84b4f9e18423f
Simon Francies <simon_francies@hotmail.com>
2019-03-31 19:02:11 +0100

    Add controller unit tests

--- ShoppingCartApi/test/ShoppingCartApi.UnitTests/ControllerTests/OrderItemsControllerTests.cs
+++ ShoppingCartApi/test/ShoppingCartApi.UnitTests/ControllerTests/OrderItemsControllerTests.cs
@@ -1,11 +1,219 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using ShoppingCartApi.Api.Controllers;
+using ShoppingCartApi.Common.Exceptions;
+using ShoppingCartApi.Common.Interface;
+using ShoppingCartApi.Common.Models;
+using Xunit;
 
 namespace ShoppingCartApi.UnitTests.ControllerTests
 {
     public class OrderItemsControllerTests
     {
+        private readonly Mock<IOrderItemAccess> _mockOrderItemAccess = new Mock<IOrderItemAccess>();
+        private readonly Fixture _fixture = new Fixture();
 
+        [Fact]
+        public void GetOrderItemReturnsOkayWhenOrderIsFound()
+        {
+            OrderItem orderItem = _fixture.Create<OrderItem>();
+            _mockOrderItemAccess.Reset();
+            _mockOrderItemAccess.Setup(a => a.GetOrderItem(It.IsAny<Guid>())).Returns(orderItem);
+            OrderItemsController sut = CreateSystemUnderTest();
+
+            var result = sut.Get(It.IsAny<Guid>());
+
+            _mockOrderItemAccess.Verify(a => a.GetOrderItem(It.IsAny<Guid>()), Times.Once);
+            Assert.IsType<OkObjectResult>(result);
+            Assert.NotNull((OrderItem)((OkObjectResult)result).Value);
+        }
+
+        [Fact]
+        public void GetOrderItemReturns404WhenOrderNotFound()
+        {
+            _mockOrderItemAccess.Reset();
+            _mockOrderItemAccess.Setup(a => a.GetOrderItem(It.IsAny<Guid>())).Returns((OrderItem)null);
+            OrderItemsController sut = CreateSystemUnderTest();
+
+            var result = sut.Get(It.IsAny<Guid>());
+
+            _mockOrderItemAccess.Verify(a => a.GetOrderItem(It.IsAny<Guid>()), Times.Once);
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task PostOrderItemReturnsOkayWhenOrderItemCreated()
+        {
+            OrderItem orderItem = _fixture.Create<OrderItem>();
+            Guid orderId = Guid.NewGuid();
+            Guid productId = Guid.NewGuid();
+            int quantity = _fixture.Create<int>();
+            _mockOrderItemAccess.Reset();
+            _mockOrderItemAccess
+                .Setup(a => a.CreateOrderItemAsync(orderId, productId, quantity, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(orderItem);
+            OrderItemsController sut = CreateSystemUnderTest();
+
+            var result = await sut.PostAsync(orderId, productId, quantity, It.IsAny<CancellationToken>());
+
+            _mockOrderItemAccess.Verify(a => a.CreateOrderItemAsync(orderId, productId, quantity, It.IsAny<CancellationToken>()), Times.Once);
+            Assert.IsType<OkObjectResult>(result);
+            Assert.NotNull((OrderItem)((OkObjectResult)result).Value);
+        }
+
+        [Fact]
+        public async Task PostOrderItemReturns400WhenQuantityLessThan1()
+        {
+            OrderItem orderItem = _fixture.Create<OrderItem>();
+            Guid orderId = Guid.NewGuid();
+            Guid productId = Guid.NewGuid();
+            int quantity = 0;
+            _mockOrderItemAccess.Reset();
+            _mockOrderItemAccess
+                .Setup(a => a.CreateOrderItemAsync(orderId, productId, quantity, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(orderItem);
+            OrderItemsController sut = CreateSystemUnderTest();
+
+            var result = await sut.PostAsync(orderId, productId, quantity, It.IsAny<CancellationToken>());
+
+            _mockOrderItemAccess.Verify(a => a.CreateOrderItemAsync(orderId, productId, quantity, It.IsAny<CancellationToken>()), Times.Never);
+            Assert.IsType<BadRequestResult>(result);
+        }
+
+        [Fact]
+        public async Task PostOrderItemReturns404WhenOrderNotFound()
+        {
+            OrderItem orderItem = _fixture.Create<OrderItem>();
+            Guid orderId = Guid.NewGuid();
+            Guid productId = Guid.NewGuid();
+            int quantity = _fixture.Create<int>();
+            _mockOrderItemAccess.Reset();
+            _mockOrderItemAccess
+                .Setup(a => a.CreateOrderItemAsync(orderId, productId, quantity, It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new OrderNotFoundException(string.Empty));
+            OrderItemsController sut = CreateSystemUnderTest();
+
+            var result = await sut.PostAsync(orderId, productId, quantity, It.IsAny<CancellationToken>());
+
+            _mockOrderItemAccess.Verify(a => a.CreateOrderItemAsync(orderId, productId, quantity, It.IsAny<CancellationToken>()), Times.Once);
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task PostOrderItemReturns404WhenProductNotFound()
+        {
+            OrderItem orderItem = _fixture.Create<OrderItem>();
+            Guid orderId = Guid.NewGuid();
+            Guid productId = Guid.NewGuid();
+            int quantity = _fixture.Create<int>();
+            _mockOrderItemAccess.Reset();
+            _mockOrderItemAccess
+                .Setup(a => a.CreateOrderItemAsync(orderId, productId, quantity, It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new ProductNotFoundException(string.Empty));
+            OrderItemsController sut = CreateSystemUnderTest();
+
+            var result = await sut.PostAsync(orderId, productId, quantity, It.IsAny<CancellationToken>());
+
+            _mockOrderItemAccess.Verify(a => a.CreateOrderItemAsync(orderId, productId, quantity, It.IsAny<CancellationToken>()), Times.Once);
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task PutOrderItemReturnsOkayWhenOrderItemUpdated()
+        {
+            OrderItem orderItem = _fixture.Create<OrderItem>();
+            Guid orderItemId = Guid.NewGuid();
+            int quantity = _fixture.Create<int>();
+            _mockOrderItemAccess.Reset();
+            _mockOrderItemAccess
+                .Setup(a => a.UpdateOrderItemQuantityAsync(orderItemId, quantity, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(orderItem);
+            OrderItemsController sut = CreateSystemUnderTest();
+
+            var result = await sut.PutAsync(orderItemId, quantity, It.IsAny<CancellationToken>());
+
+            _mockOrderItemAccess.Verify(a => a.UpdateOrderItemQuantityAsync(orderItemId, quantity, It.IsAny<CancellationToken>()), Times.Once);
+            Assert.IsType<OkObjectResult>(result);
+            Assert.NotNull((OrderItem)((OkObjectResult)result).Value);
+        }
+
+        [Fact]
+        public async Task PutOrderItemReturns400WhenQuantityLessThan1()
+        {
+            OrderItem orderItem = _fixture.Create<OrderItem>();
+            Guid orderItemId = Guid.NewGuid();
+            int quantity = 0;
+            _mockOrderItemAccess.Reset();
+            _mockOrderItemAccess
+                .Setup(a => a.UpdateOrderItemQuantityAsync(orderItemId, quantity, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(orderItem);
+            OrderItemsController sut = CreateSystemUnderTest();
+
+            var result = await sut.PutAsync(orderItemId, quantity, It.IsAny<CancellationToken>());
+
+            _mockOrderItemAccess.Verify(a => a.UpdateOrderItemQuantityAsync(orderItemId, quantity, It.IsAny<CancellationToken>()), Times.Never);
+            Assert.IsType<BadRequestResult>(result);
+        }
+
+        [Fact]
+        public async Task PutOrderItemReturns404WhenOrderItmNotFound()
+        {
+            OrderItem orderItem = _fixture.Create<OrderItem>();
+            Guid orderItemId = Guid.NewGuid();
+            int quantity = _fixture.Create<int>();
+            _mockOrderItemAccess.Reset();
+            _mockOrderItemAccess
+                .Setup(a => a.UpdateOrderItemQuantityAsync(orderItemId, quantity, It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new OrderItemNotFoundException(string.Empty));
+            OrderItemsController sut = CreateSystemUnderTest();
+
+            var result = await sut.PutAsync(orderItemId, quantity, It.IsAny<CancellationToken>());
+
+            _mockOrderItemAccess.Verify(a => a.UpdateOrderItemQuantityAsync(orderItemId, quantity, It.IsAny<CancellationToken>()), Times.Once);
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task DeleteOrderItemReturnsOkayWhenOrderItemDeleted()
+        {
+            Guid orderItemId = Guid.NewGuid();
+            _mockOrderItemAccess.Reset();
+            _mockOrderItemAccess.Setup(a => a.RemoveOrderItemAsync(orderItemId, It.IsAny<CancellationToken>()))
+                .Returns(Task.CompletedTask);
+
+            OrderItemsController sut = CreateSystemUnderTest();
+
+            var result = await sut.DeleteAsync(orderItemId, It.IsAny<CancellationToken>());
+
+            _mockOrderItemAccess.Verify(a => a.RemoveOrderItemAsync(orderItemId, It.IsAny<CancellationToken>()), Times.Once);
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public async Task DeleteOrderItemReturns404WhenOrderItmNotFound()
+        {
+            Guid orderItemId = Guid.NewGuid();
+            _mockOrderItemAccess.Reset();
+            _mockOrderItemAccess.Setup(a => a.RemoveOrderItemAsync(orderItemId, It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new OrderItemNotFoundException(string.Empty));
+
+            OrderItemsController sut = CreateSystemUnderTest();
+
+            var result = await sut.DeleteAsync(orderItemId, It.IsAny<CancellationToken>());
+
+            _mockOrderItemAccess.Verify(a => a.RemoveOrderItemAsync(orderItemId, It.IsAny<CancellationToken>()), Times.Once);
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        private OrderItemsController CreateSystemUnderTest()
+        {
+            return new OrderItemsController(_mockOrderItemAccess.Object);
+        }
     }
 }
--- ShoppingCartApi/test/ShoppingCartApi.UnitTests/ControllerTests/OrdersControllerTests.cs
+++ ShoppingCartApi/test/ShoppingCartApi.UnitTests/ControllerTests/OrdersControllerTests.cs
@@ -1,10 +1,160 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using ShoppingCartApi.Api.Controllers;
+using ShoppingCartApi.Common.Exceptions;
+using ShoppingCartApi.Common.Interface;
+using ShoppingCartApi.Common.Models;
+using Xunit;
 
 namespace ShoppingCartApi.UnitTests.ControllerTests
 {
     public class OrdersControllerTests
     {
+        private readonly Mock<IOrderAccess> _mockOrderAccess = new Mock<IOrderAccess>();
+        private readonly Fixture _fixture = new Fixture();
+
+        [Fact]
+        public void GetReturnsOkayWhenOrdersAreFound()
+        {
+            List<Order> orders = _fixture.CreateMany<Order>(10).ToList();
+            _mockOrderAccess.Reset();
+            _mockOrderAccess.Setup(a => a.GetAllOrders()).Returns(orders);
+            OrdersController sut = CreateSystemUnderTest();
+
+            var result = sut.Get();
+
+            _mockOrderAccess.Verify(a => a.GetAllOrders(), Times.Once);
+            Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(orders.Count, ((IReadOnlyCollection<Order>)((OkObjectResult)result).Value).Count);
+        }
+
+        [Fact]
+        public void GetReturns404WhenNoOrdersFound()
+        {
+            _mockOrderAccess.Reset();
+            _mockOrderAccess.Setup(a => a.GetAllOrders()).Returns(new List<Order>());
+            OrdersController sut = CreateSystemUnderTest();
+
+            var result = sut.Get();
+
+            _mockOrderAccess.Verify(a => a.GetAllOrders(), Times.Once);
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public void GetOrderReturnsOkayWhenOrderIsFound()
+        {
+            Order order = _fixture.Create<Order>();
+            _mockOrderAccess.Reset();
+            _mockOrderAccess.Setup(a => a.GetOrder(order.Id)).Returns(order);
+            OrdersController sut = CreateSystemUnderTest();
+
+            var result = sut.Get(order.Id);
+
+            _mockOrderAccess.Verify(a => a.GetOrder(order.Id), Times.Once);
+            Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(order.Id, ((Order)((OkObjectResult)result).Value).Id);
+        }
+
+        [Fact]
+        public void GetOrderReturns404WhenOrderNotFound()
+        {
+            _mockOrderAccess.Reset();
+            _mockOrderAccess.Setup(a => a.GetOrder(It.IsAny<Guid>())).Returns((Order)null);
+            OrdersController sut = CreateSystemUnderTest();
+
+            var result = sut.Get(It.IsAny<Guid>());
+
+            _mockOrderAccess.Verify(a => a.GetOrder(It.IsAny<Guid>()), Times.Once);
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public void GetReturnsOkayWhenCustomerOrdersAreFound()
+        {
+            List<Order> orders = _fixture.CreateMany<Order>(10).ToList();
+            Guid customerId = Guid.NewGuid();
+            _mockOrderAccess.Reset();
+            _mockOrderAccess.Setup(a => a.GetCustomerOrders(customerId)).Returns(orders);
+            OrdersController sut = CreateSystemUnderTest();
+
+            var result = sut.GetCustomerOrders(customerId);
+
+            _mockOrderAccess.Verify(a => a.GetCustomerOrders(customerId), Times.Once);
+            Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(orders.Count, ((IReadOnlyCollection<Order>)((OkObjectResult)result).Value).Count);
+        }
+
+        [Fact]
+        public void GetReturns404WhenNoCustomerOrdersFound()
+        {
+            _mockOrderAccess.Reset();
+            Guid customerId = Guid.NewGuid();
+            _mockOrderAccess.Setup(a => a.GetCustomerOrders(customerId)).Returns(new List<Order>());
+            OrdersController sut = CreateSystemUnderTest();
+
+            var result = sut.GetCustomerOrders(customerId);
+
+            _mockOrderAccess.Verify(a => a.GetCustomerOrders(customerId), Times.Once);
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task PostReturnsOkayWhenCustomerExists()
+        {
+            Order order = _fixture.Create<Order>();
+            _mockOrderAccess.Reset();
+            Guid customerId = Guid.NewGuid();
+            _mockOrderAccess.Setup(a => a.CreateOrderAsync(customerId, It.IsAny<CancellationToken>())).ReturnsAsync(order);
+            OrdersController sut = CreateSystemUnderTest();
+
+            var result = await sut.PostAsync(customerId);
+
+            _mockOrderAccess.Verify(a => a.CreateOrderAsync(customerId, It.IsAny<CancellationToken>()), Times.Once);
+            Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(order.Id, ((Order)((OkObjectResult)result).Value).Id);
+        }
+
+        [Fact]
+        public async Task PostReturns404WhenCustomerNotFound()
+        {
+            Order order = _fixture.Create<Order>();
+            _mockOrderAccess.Reset();
+            Guid customerId = Guid.NewGuid();
+            _mockOrderAccess.Setup(a => a.CreateOrderAsync(customerId, It.IsAny<CancellationToken>())).ThrowsAsync(new CustomerNotFoundException(string.Empty));
+            OrdersController sut = CreateSystemUnderTest();
+
+            var result = await sut.PostAsync(customerId);
+
+            _mockOrderAccess.Verify(a => a.CreateOrderAsync(customerId, It.IsAny<CancellationToken>()), Times.Once);
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task DeleteReturnsOkay()
+        {
+            Order order = _fixture.Create<Order>();
+            _mockOrderAccess.Reset();
+            Guid orderId = Guid.NewGuid();
+            OrdersController sut = CreateSystemUnderTest();
+
+            var result = await sut.DeleteAsync(orderId);
+
+            _mockOrderAccess.Verify(a => a.ClearOrderAsync(orderId, It.IsAny<CancellationToken>()), Times.Once);
+            Assert.IsType<OkResult>(result);
+        }
+
+
+        private OrdersController CreateSystemUnderTest()
+        {
+            return new OrdersController(_mockOrderAccess.Object);
+        }
     }
 }
